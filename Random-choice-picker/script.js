6edf624d80c4c26ec51e3007b89f168e812990c2
Shubham Singh <87699417+detonators@users.noreply.github.com>
2021-12-17 16:53:18 +0530

    Update script.js

--- Random-choice-picker/script.js
+++ Random-choice-picker/script.js
@@ -1,7 +1,14 @@
+//to get the tag element from the html file
+
 const tagsEl = document.getElementById('tags')
+
+//now for text area
+
 const textarea = document.getElementById('textarea')
 
 textarea.focus()
+
+// to leastion to keyup or key down
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
@@ -9,17 +16,25 @@ textarea.addEventListener('keyup', (e) => {
     if(e.key === 'Enter') {
         setTimeout(() => {
             e.target.value = ''
-        }, 10)
+        }, 10) // clearing the input 
 
         randomSelect()
     }
 })
 
 function createTags(input) {
+	
+	
+    //used split to make the letters on ether side of the comma an array.
+	
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
     
+    // filter to remove empty space.
+    
     tagsEl.innerHTML = ''
-
+	
+// Taking the element and using foreach to loop through it
+	
     tags.forEach(tag => {
         const tagEl = document.createElement('span')
         tagEl.classList.add('tag')
@@ -27,9 +42,13 @@ function createTags(input) {
         tagsEl.appendChild(tagEl)
     })
 }
+// To make random selections
 
 function randomSelect() {
     const times = 30
+
+
+    // created new variable 
 
     const interval = setInterval(() => {
         const randomTag = pickRandomTag()
@@ -43,6 +62,8 @@ function randomSelect() {
 	}
     }, 100);
 
+// Defined the time, how much it will take to highlight and to unhighlight the tags.
+
     setTimeout(() => {
         clearInterval(interval)
 
@@ -55,14 +76,19 @@ function randomSelect() {
     }, times * 100)
 }
 
+// Used Query to select all the options.
+
 function pickRandomTag() {
     const tags = document.querySelectorAll('.tag')
     return tags[Math.floor(Math.random() * tags.length)]
 }
 
+// function to highlight
+
 function highlightTag(tag) {
     tag.classList.add('highlight')
 }
+// To un highlight the tags
 
 function unHighlightTag(tag) {
     tag.classList.remove('highlight')
