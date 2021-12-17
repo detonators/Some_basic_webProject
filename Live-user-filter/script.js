768589020e6fc12a79969dc1026c4207fad3bc52
Shubham Singh <87699417+detonators@users.noreply.github.com>
2021-12-17 17:49:37 +0530

    Update script.js

--- Live-user-filter/script.js
+++ Live-user-filter/script.js
@@ -1,22 +1,31 @@
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = []
-
+//
 getData()
 
+//Filter for data
 filter.addEventListener('input', (e) => filterData(e.target.value))
+ 
+
+// creating a asynchronous function 
 
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50')
-
+              
+    //API used here to get random user data
+    
     const { results } = await res.json()
 
     // Clear result
+    
     result.innerHTML = ''
 
     results.forEach(user => {
         const li = document.createElement('li')
 
+        // creating the list items for each users
+        
         listItems.push(li)
 
         li.innerHTML = `
@@ -26,10 +35,13 @@ async function getData() {
                 <p>${user.location.city}, ${user.location.country}</p>
             </div>
         `
+        //user Name and location is being fetched
 
-        result.appendChild(li)
+        result.appendChild(li)  //Putting it in the result
     })
 }
+
+//Function for filtered data..................
 
 function filterData(searchTerm) {
     listItems.forEach(item => {
